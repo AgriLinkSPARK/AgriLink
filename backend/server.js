e658c8db7c03713031abddbae553e5db716ab6df
Aenuka Buddhakorala <aenuka.elakollek@gmail.com>
2026-02-27 23:59:24 +0530

    final commit 80% backend

--- backend/server.js
+++ backend/server.js
@@ -77,3 +77,5 @@ app.use(errorMiddleware);
 // ─── Start server ─────────────────────────────────────────────────────────────
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🔴 Server running on port ${PORT}`));
+
+// final commit 80% backend 
